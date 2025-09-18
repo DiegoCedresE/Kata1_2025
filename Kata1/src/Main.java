abb685d86761d563f4bf51a48a82fe139f966847
DiegoCedresE <diego.cedres101@alu.ulpgc.es>
2025-09-18 10:27:39 +0100

    Proyecto kata 1

--- Kata1/src/Main.java
+++ Kata1/src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+/*
+Proyecto de kata 1 realizad
+ */
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+package kata1;
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LocalDate date;
+        date = LocalDate.of(1949, 9, 24);
+
+        kata1.Person person = new kata1.Person("Blas", "Cabrera", date);
+        System.out.println(person.getName() + " " + person.getSurname() + " tiene " + person.getAge() + " años");
     }
+
 }
---
+++ Kata1/src/Person.java
@@ -0,0 +1,42 @@
+
+package kata1;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Person {
+    private final String name;
+    private final String surname;
+    private final LocalDate birthdate;
+    private final long MILLISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
+
+
+    public Person(String name, String surname, LocalDate birthdate) {
+        this.name = name;
+        this.surname = surname;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public int getAge() {
+        LocalDate today = LocalDate.now();
+        return Period.between(birthdate, today).getYears();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+
+}
